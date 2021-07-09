2edf360edf3b85343ecdc6d33803ce270ed6c359
martinbannister <martin.bannister91@gmail.com>
2021-07-09 20:19:31 +0100

    feat(script): finalised code for minus & times games in runGame() and calcualteCorrectAnswer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,7 +91,7 @@ function calculateCorrectAnswer() {
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
     } else if (operator === 'x') {
-        return [operand1 * operand2, 'subtract'];
+        return [operand1 * operand2, 'multiply'];
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}, aborting!`;
@@ -124,8 +124,8 @@ function displayAdditonQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = '-';
 
 
