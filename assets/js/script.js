7d29101433aec2a83097e2cf40cd293795ce3b6a
martinbannister <martin.bannister91@gmail.com>
2021-07-09 20:49:57 +0100

    feat(script): division - add core division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,6 +101,8 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, 'subtract'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === '÷') {
+        return [operand1 / operand2, 'division'];
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}, aborting!`;
@@ -151,4 +153,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '÷';
 }
